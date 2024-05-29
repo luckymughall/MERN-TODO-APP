a8553950afbfd1bfd6184b102e999acd4df03ffb
Luqman Jafir <47683436+luckymughall@users.noreply.github.com>
2024-05-29 12:24:16 +0500

    Create AdComponent.jsx

---
+++ frontend/src/components/AdComponent.jsx
@@ -0,0 +1,33 @@
+import React, { useEffect } from 'react';
+
+const AdComponent = () => {
+  useEffect(() => {
+    const script = document.createElement('script');
+    script.async = true;
+    script.src = 'https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-5769093471889222';
+    script.crossOrigin = 'anonymous';
+    document.body.appendChild(script);
+
+    return () => {
+      document.body.removeChild(script);
+    };
+  }, []);
+
+  useEffect(() => {
+    if (window.adsbygoogle) {
+      window.adsbygoogle.push({});
+    }
+  }, []);
+
+  return (
+    <div>
+      <ins className="adsbygoogle"
+           style={{ display: 'block' }}
+           data-ad-client="ca-pub-5769093471889222"
+           data-ad-slot="YOUR_AD_SLOT"
+           data-ad-format="auto"></ins>
+    </div>
+  );
+};
+
+export default AdComponent;
